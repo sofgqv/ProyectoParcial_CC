db39b02009801b25c0657f7d099b823521489137
ValDLaw <76666534+ValDLaw@users.noreply.github.com>
2022-09-25 15:50:44 -0500

    Draft - Donar frontend

---
+++ llevame_frontend/src/components/ApiVerDonaciones.vue
@@ -0,0 +1,44 @@
+<!--
+  <template>
+  <div>
+    <ul v-if="donaciones && donaciones.length">
+      <li v-for="donacion of donaciones">
+        <p><strong>{{donacion.monto}}</strong></p>
+        <p>{{donacion.nombres}}</p>
+        <p>{{donacion.apellidos}}</p>
+      </li>
+    </ul>
+
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors">
+        {{error.message}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name : 'ApiVerDonaciones',
+  data() {
+    return {
+      donaciones: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(``)
+    .then(response => {
+      this.donaciones = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
+-->
