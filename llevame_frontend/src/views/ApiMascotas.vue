7e87cffc25badbc679c3941b20e472e1fca78502
ValDLaw <76666534+ValDLaw@users.noreply.github.com>
2022-09-30 07:27:12 -0500

    Mascota get + post

--- llevame_frontend/src/views/ApiMascotas.vue
+++ llevame_frontend/src/views/ApiMascotas.vue
@@ -1,41 +1,98 @@
 <template>
-  <div>
-    <ul v-if="mascotas">
-      <li v-for="mascota of mascotas" :key="mascota">
-        <p><strong>{{mascota.title}}</strong></p>
-        <p>{{mascota.body}}</p>
-      </li>
-    </ul>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap/dist/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css" />
+  </head>
 
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors" :key="error">
-        {{error.message}}
-      </li>
-    </ul>
-  </div>
+    <div class="container" id="vuejscrudapp">
+        <div class="row">
+            <div class="col-md-12 mt-5">
+            <h1 class="text-center">Mascotas</h1>
+            <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            <!-- Add Records -->
+            <div>
+                <h4 id="show-btn" @click="showModal('my-modal')">¡Registro de mascotas!</h4>
+    
+                <div ref="my-modal" hide-footer title="nueva mascota">
+                <div>
+                    <form action="" @submit.prevent="onSubmit">
+                    <div class="form-group">
+                        <label for="">Nombre</label>
+                        <input type="text" v-model="nombre" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Raza</label>
+                        <input type="text" v-model="raza" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Fecha de nacimiento</label>
+                        <input type="text" v-model="fecha_n" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Sexo</label>
+                        <input type="text" v-model="sexo" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Size</label>
+                        <input type="text" v-model="size" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-sm btn-outline-info">Inscribirme</button>
+                    </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
-  
+    
 <script>
 import axios from 'axios';
 
 export default {
   data() {
     return {
-      mascotas: [],
+      nombre: '',
+      raza: '',
+      fecha_n: '',
+      sexo: '',
+      size: '',
       errors: []
+    }},
+  methods: {
+    onSubmit(){
+      if (this.nombre !== '' && this.raza !== '' && this.fecha_n !== '' && this.sexo !== '' && this.size !== '') {
+        axios.post("",{
+          nombre : this.nombre,
+          raza : this.raza,
+          fecha_n : this.fecha_n,
+          sexo : this.sexo,
+          size : this.size
+        },
+        {headers: {  
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'}
+        })
+        .then(res => {
+            console.log(res)
+            alert('¡Mascota añadida!')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }else{
+        alert('No se pudo añadir. Inténtalo nuevamente.')
+      }
     }
-  },
-
-  // Fetches posts when the component is created.
-  created() {
-    axios.get(``)
-    .then(response => {
-      // JSON responses are automatically parsed.
-      this.mascotas = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
   }
 }
+
 </script>
---
+++ llevame_frontend/src/views/ApiVerMascotas.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+        <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Raza</th>
+                <th>FN</th>
+                <th>Sexo</th>
+                <th>Size</th>
+                <th>Estado de adopción</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="mascota in mascotas" :key="mascota.id">
+                <td>{{mascota.id}}</td>
+                <td>{{mascota.nombre}}</td>
+                <td>{{mascota.raza}}</td>
+                <td>{{mascota.fecha_n}}</td>
+                <td>{{mascota.sexo}}</td>
+                <td>{{mascota.size}}</td>
+                <td>{{mascota.estado_adop}}</td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+</template>
+    
+<script>
+import axios from 'axios';
+
+export default {
+    el: '#app',
+    data() {
+    return {
+        mascotas: ''
+    }},
+    mounted(){
+        axios.get('',
+        {headers: {  
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'}
+        })
+        .then(res => {
+            this.mascotas = res.data
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+}
+
+</script>
+  
